3e3c152ac99415196508db5ff99a48b36243e8fe
jeremy.zhang <zhangyue9306@gmail.com>
2020-02-13 11:42:02 +0800

    add compiler

---
+++ Inbox/compile.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+// https://stackoverflow.com/questions/53353167/npm-solc-assertionerror-err-assertion-invalid-callback-specified
+// export the compiled module
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
