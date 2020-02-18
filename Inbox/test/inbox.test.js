3381ee416cfaa8f843bca88894895effdbb957ec
jeremy.zhang <zhangyue9306@gmail.com>
2020-02-18 11:19:52 +0800

    test deploy contract

--- Inbox/test/inbox.test.js
+++ Inbox/test/inbox.test.js
@@ -3,8 +3,26 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const mocha = require('mocha');
+const {interface, bytecode} = require('../compile');
 
+let accounts;
+let inbox;
 
+mocha.beforeEach(async () => {
+    // get list of accounts
+    accounts = await web3.eth.getAccounts();
+
+    // deploy a contract to an account
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hello']})
+        .send({from: accounts[0], gas: '1000000'})
+});
+
+mocha.describe('Inbox', () => {
+    mocha.it('deploy a contract', () => {
+        console.log(inbox)
+    });
+});
 
 
 
