6011748f2cec641e9c17e67b92721f1126184e8d
jeremy.zhang <zhangyue9306@gmail.com>
2020-03-27 11:16:50 +0800

    deployed contract

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -4,6 +4,6 @@ const CampaignFactory = require("./build/CampaignFactory");
 
 // load the deployed factory instance for further import
 const campaignAddress = '0x0cA24B23981cE5a42a5ce5741DF078c77b2b682A';
-const instance = web3.eth.Contract(JSON.parse(CampaignFactory.interface), campaignAddress);
+const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), campaignAddress);
 
 export default instance;
--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -1,7 +1,17 @@
 import Web3 from "web3";
 
-// extract the provider from default web3 provided by the browser
-// inject it into our web3 of different version
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+// typeof check if variable exists
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // if it's in the browser and has metamask installed
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // connect to rinkeby network through infura node
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/50266a515df44177bde45f83f877503d'
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
