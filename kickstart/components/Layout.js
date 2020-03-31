65b5ef73cf27ffceb1280c2ca49c4367e7823715
jeremy.zhang <zhangyue9306@gmail.com>
2020-03-31 11:21:25 +0800

    add heat

--- kickstart/components/Layout.js
+++ kickstart/components/Layout.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from "./Header";
+import Head from "next/head"
 import {Container} from "semantic-ui-react";
 
+// Head tag place content inside of it to the top
 export default (props) => {
     // place everything inside a container so that width is restricted
     return (
         <Container>
+            <Head>
+                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+            </Head>
+
             <Header/>
             {props.children}
         </Container>
