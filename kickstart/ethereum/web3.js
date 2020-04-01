b5e6ebe37a477728f69f7134bd9b516cb9a57d64
jeremy.zhang <zhangyue9306@gmail.com>
2020-04-01 12:05:29 +0800

    console log accounts

--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -6,8 +6,16 @@ let web3;
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
     // if it's in the browser and has metamask installed
     web3 = new Web3(window.web3.currentProvider);
+    console.log("Metamask used");
+    const getAccounts = async () => {
+        const accounts = await web3.eth.getAccounts();
+        return accounts;
+    };
+
+    console.log('first account is ', getAccounts()[0]);
 } else {
     // connect to rinkeby network through infura node
+    console.log('not Metamask');
     const provider = new Web3.providers.HttpProvider(
         'https://rinkeby.infura.io/v3/50266a515df44177bde45f83f877503d'
     );
