5b87971ab6f2d82068f6b31a65606a4ac68d32aa
jeremy.zhang <zhangyue9306@gmail.com>
2020-04-14 11:02:50 +0800

    add new routes

--- kickstart/routes.js
+++ kickstart/routes.js
@@ -4,6 +4,8 @@ const routes = require('next-routes')();
 // for all route of campaigns/* route to show.js
 routes
     .add('/campaigns/new', '/campaigns/new')
-    .add('/campaigns/:address', '/campaigns/show');
+    .add('/campaigns/:address', '/campaigns/show')
+    .add('/campaigns/:address/requests', '/campaigns/requests/index')
+    .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
 
 module.exports = routes;
