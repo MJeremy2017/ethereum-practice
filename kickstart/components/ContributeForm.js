b36add88710d29b9ad5be48ddd2982e14f51cf4c
jeremy.zhang <zhangyue9306@gmail.com>
2020-04-14 11:20:14 +0800

    update cf

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -1,55 +1,54 @@
-import React, {Component} from 'react';
-import { Button, Form, Input, Message } from 'semantic-ui-react'
-import Campaign from "../ethereum/campaign";
-import web3 from "../ethereum/web3";
-import { Router } from "next-routes/typings/tests/basic";
-
+import React, { Component } from 'react';
+import { Form, Input, Message, Button } from 'semantic-ui-react';
+import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
     state = {
         value: '',
-        errMessage: '',
+        errorMessage: '',
         loading: false
     };
 
     onSubmit = async event => {
-      event.preventDefault();
-      // initiate the campaign with address passed in from props
-      const campaign = Campaign(this.props.address);
+        event.preventDefault();
 
-      this.setState({loading: true, errMessage: ''});
+        const campaign = Campaign(this.props.address);
 
-      try {
-          const accounts = web3.eth.getAccounts();
-          await campaign.methods.contribute().send({
-              from: accounts[0],
-              value: web3.utils.toWei(this.state.value, 'ether')
-          });
-          // refresh the page once finished
-          Router.replaceRoute(`/campaigns/${this.props.address}`);
-      } catch (err) {
-          this.setState({errMessage: err.message});
-      }
+        this.setState({ loading: true, errorMessage: '' });
 
-      this.setState({loading: false, value: ''});
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
 
+            Router.replaceRoute(`/campaigns/${this.props.address}`);
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({ loading: false, value: '' });
     };
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit} error={!!this.state.errMessage}>
-                <Form.Feild>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+                <Form.Field>
                     <label>Amount to Contribute</label>
                     <Input
                         value={this.state.value}
-                        onChange={event => this.setState({value: this.target.value})}
+                        onChange={event => this.setState({ value: event.target.value })}
                         label="ether"
                         labelPosition="right"
                     />
-                </Form.Feild>
-                <Message error header={'Oops!'} content={this.state.errMessage} />
+                </Form.Field>
+
+                <Message error header="Oops!" content={this.state.errorMessage} />
                 <Button primary loading={this.state.loading}>
-                    Contribute
+                    Contribute!
                 </Button>
             </Form>
         );
