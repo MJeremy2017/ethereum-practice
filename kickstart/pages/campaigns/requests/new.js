641264e4632823c947c206fbcbb2ed7fbc5e7e39
jeremy.zhang <zhangyue9306@gmail.com>
2020-04-20 11:02:56 +0800

    add req new

---
+++ kickstart/pages/campaigns/requests/new.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import Layout from "../../../components/Layout";
+import {Form, Button, Message, Input} from "semantic-ui-react";
+import Campaign from "../../../ethereum/campaign";
+import web3 from "../../../ethereum/web3";
+import {Link, Router} from "../../../routes";
+
+class RequestNew extends Component {
+    state = {
+        description: '',
+        value: '',
+        recipient: '',
+        loading: false,
+        errMessage: ''
+    };
+
+    static async getInitialProps(props) {
+        const {address} = props.query;
+
+        return {address};
+    }
+
+    onSubmit = async event => {
+        event.preventDefault();
+        // when click onSubmit, start loading
+        this.setState({ loading: true, errMessage: '' });
+
+        const {description, value, recipient} = this.state;
+        const campaign = Campaign(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.createRequest(
+                description,
+                web3.utils.toWei(value, 'ether'),
+                recipient
+            ).send({from: accounts[0]});
+
+            // after successfully submit, get back to request
+            Router.push(`/campaigns/${this.props.address}/requests`);
+        } catch (err) {
+            this.setState({ errMessage: err.message });
+        }
+
+        this.setState({ loading: false })
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                    <a>Back</a>
+                </Link>
+                <h3>Create a Request</h3>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errMessage}>
+                    <Form.Field>
+                        <label>Description</label>
+                        <Input
+                            value={this.state.description}
+                            onChange={event => this.setState({description: event.target.value})}
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Value in Either</label>
+                        <Input
+                            value={this.state.value}
+                            onChange={event => this.setState({value: event.target.value})}
+                            label={"ether"}
+                            labelPosition={"right"}
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Recipient</label>
+                        <Input
+                            value={this.state.recipient}
+                            onChange={event => this.setState({recipient: event.target.value})}
+                        />
+                    </Form.Field>
+
+                    <Message error header={"Oops!"} content={this.state.errMessage} />
+                    <Button primary loading={this.state.loading}>Create</Button>
+                </Form>
+            </Layout>
+        );
+    }
+}
+
+export default RequestNew;
